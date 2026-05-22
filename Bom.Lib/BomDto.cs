5ef7c65cc3cdaa15b357dbdba75f9be6616986af
Berat <wrilles7894@gmail.com>
2026-05-22 21:34:58 +0300

    v0.0.2 - successfully published to local server, fixed runtime module loading and resolved 500 error

---
+++ Bom.Lib/BomDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Bom.Lib
+{
+    public class BomDto
+    {
+        public string RecipeCode { get; set; } = string.Empty;
+        public string ProductCode { get; set; } = string.Empty;
+        public string RecipeName { get; set; } = string.Empty;
+    }
+}
